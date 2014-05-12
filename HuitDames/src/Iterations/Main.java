1271804a11792c2ed2b06e9e7a345c2444080c43
Gaet27 <dion.gaetan@gmail.com>
2014-05-12 17:57:23 +0200

    Création de l'IHM

--- HuitDames/src/Iterations/Main.java
+++ HuitDames/src/Iterations/Main.java
@@ -1,10 +1,13 @@
 package Iterations;
 
+import Graphique.FenetreEchiquier;
+
 public class Main {
 
 
 
 	public static void main(String[] args) {
+		FenetreEchiquier fenetre = new FenetreEchiquier();
 		
 	}
 
